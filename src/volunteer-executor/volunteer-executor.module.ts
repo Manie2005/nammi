50c893b948849a301194a97217baff2b6db69409
Your Name~Manie2005 <maniesamuel24@gmail.com>
2025-03-13 17:36:27 +0100

    Latest Commit

--- src/volunteer-executor/volunteer-executor.module.ts
+++ src/volunteer-executor/volunteer-executor.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { VolunteerExecutorController } from './volunteer-executor.controller';
 import { VolunteerExecutorService } from './volunteer-executor.service';
-
+import { Executor } from 'src/entity/executor.entity';
+import { Task } from 'src/entity/task.entity';
+import { User } from 'src/entity/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
+  imports: [TypeOrmModule.forFeature([Executor, Task, User])],
   controllers: [VolunteerExecutorController],
   providers: [VolunteerExecutorService]
 })
--- src/volunteer-executor/volunteer-executor.service.ts
+++ src/volunteer-executor/volunteer-executor.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Executor } from 'src/entity/executor.entity';
+import { Task } from 'src/entity/task.entity';
+import { User } from 'src/entity/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class VolunteerExecutorService {
     constructor(
-        
+        @InjectRepository(Executor) private readonly executorRepository: Repository<Executor>,
+        @InjectRepository(Task) private readonly taskRepository: Repository<Task>,
+        @InjectRepository(User) private readonly userRepository: Repository<User>
     ){}
 }
